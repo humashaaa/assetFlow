c0db3804a3fb0048e2943e0857857a7ad7d96796
humashaaa <afridakhan2121@gmail.com>
2024-11-28 20:45:25 +0600

    footer

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,23 +3,123 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     const current = new Date().getFullYear()
     return (
-        <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
-  <nav className="grid grid-flow-col gap-4">
-    <Link to='/' className="link link-hover">About us</Link>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-  </nav> 
-  <nav>
-    <div className="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-    </div>
-  </nav> 
-  <aside>
-    <p>Copyright © {current} - All right reserved by ACME Industries Ltd</p>
-  </aside>
-</footer>
+      <footer className="bg-[#030C0D]">
+      <div className="mx-auto max-w-screen-xl px-4 pb-5 pt-16 sm:px-6 lg:px-8 lg:pt-20">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold text-white sm:text-5xl">Partner in Asset Management</h2>
+    
+          <p className="mx-auto mt-5 w-3/4 text-gray-500">
+          At AssetFlow, we provide the tools you need to track, manage, and optimize your assets effortlessly. Our platform is designed to bring clarity and confidence to your financial decisions. Let us be your trusted partner in achieving your goals, one asset at a time.
+          </p>
+    
+          <Link
+            to='/HRmanager'
+            className="mt-8 inline-block rounded-full border border-blue-600 px-12 py-3 text-sm font-medium text-blue-600 hover:bg-blue-600 hover:text-white focus:outline-none focus:ring active:bg-blue-500"
+          >
+            Get Started
+          </Link>
+        </div>
+    
+        <div
+          className="mt-16 border-t border-gray-600  py-5  sm:flex sm:items-center sm:justify-between lg:mt-20"
+        >
+          <ul className="flex flex-wrap pt-3  justify-center gap-4 text-[13px] font-semibold lg:justify-end">
+            <li>
+              <Link to='/' className="text-white transition hover:opacity-75"> Terms & Conditions </Link>
+            </li>
+    
+            <li>
+              <Link to='/contact' className="text-white transition hover:opacity-75"> Contact us </Link>
+            </li>
+    
+            <li>
+              <Link to='/contact' className="text-white transition hover:opacity-75"> Support </Link>
+            </li>
+          </ul>
+    
+          <ul className="mt-8 flex pt-4 justify-center gap-6 sm:mt-0 lg:justify-end">
+            <li>
+              <Link
+                to='https://www.facebook.com/'
+                rel="noreferrer"
+                target="_blank"
+                className="text-white transition hover:opacity-75"
+              >
+                <span className="sr-only">Facebook</span>
+    
+                <svg className="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path
+                    fillRule="evenodd"
+                    d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
+            </li>
+    
+
+    
+            <li>
+              <Link
+               to='https://x.com/home?lang=en'
+                rel="noreferrer"
+                target="_blank"
+                className="text-white transition hover:opacity-75"
+              >
+                <span className="sr-only">Twitter</span>
+    
+                <svg className="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path
+                    d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+                  />
+                </svg>
+              </Link>
+            </li>
+    
+            <li>
+              <Link
+                to='https://github.com/humashaaa'
+                rel="noreferrer"
+                target="_blank"
+                className="text-white transition hover:opacity-75"
+              >
+                <span className="sr-only">GitHub</span>
+    
+                <svg className="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
+            </li>
+    
+            <li>
+              <Link
+                to='https://dribbble.com/'
+                rel="noreferrer"
+                target="_blank"
+                className="text-white transition hover:opacity-75"
+              >
+                <span className="sr-only">Dribbble</span>
+    
+                <svg className="size-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                  <path
+                    fillRule="evenodd"
+                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="text-white text-center pt-4">Copyright ©{current} - All Right Reserved by <Link className="text-blue-800 font-bold" to='/'>AssetFlow</Link></p>
+        </div>
+      </div>
+    </footer>
     );
 };
 
