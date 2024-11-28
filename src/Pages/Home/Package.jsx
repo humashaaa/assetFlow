f23fb525f7a1b59f3b537df29ef05b7d255fe3fd
humashaaa <afridakhan2121@gmail.com>
2024-11-28 16:46:12 +0600

    pricing card

--- src/Pages/Home/Package.jsx
+++ src/Pages/Home/Package.jsx
@@ -3,14 +3,14 @@ import PricingCard from "./PricingCard";
 
 const Package = () => {
   return (
-    <div>
+    <div className="">
       <Header
         title={"Packages We Offer"}
         subtitle={`Select the plan that best fits your team and
         start managing your assets more efficiently today!`}
       ></Header>
 
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-white dark:bg-gray-900 pt-12">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
             {/* Pricing Card: Starter */}
