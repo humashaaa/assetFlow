42d75a60c1906676e5edeec48725b2f55616a163
humashaaa <afridakhan2121@gmail.com>
2024-12-01 03:18:15 +0600

    dashboard

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,30 +1,94 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import useHr from "../Hooks/useHr";
-
+import { FaClipboardList } from "react-icons/fa";
+import { MdAddToPhotos } from "react-icons/md";
+import { FaWallet } from "react-icons/fa6";
+import { IoPeople } from "react-icons/io5";
+import { IoPersonAddSharp } from "react-icons/io5";
+import { RiLogoutBoxRFill } from "react-icons/ri";
+import useAuth from "../Hooks/useAuth";
 const Dashboard = () => {
-  const [isHr] = useHr()
+  const [isHr] = useHr();
+  const navigate = useNavigate()
+  const {user, logOut} = useAuth()
+  const handleLogOut =()=>{
+    logOut()
+    navigate('/')
+    
+  }
   return (
     <div className="flex">
-      <div className="w-64 min-h-screen bg-blue-100">
-        <ul className="menu">
+      <div className="w-64 min-h-screen bg-[#030C0D] pb-12 flex flex-col items-center justify-between">
+        <ul className=" text-white text-xl space-y-4 font-semibold">
+          <li>
+            <NavLink to="/" className="font-bold text-4xl mb-20">
+              Asset<span className="text-blue-500 ">Flow</span>
+            </NavLink>
+          </li>
           {isHr ? (
             <>
               <li>
-                <NavLink to="/dashboard/assetList">Asset List</NavLink>
+                <NavLink
+                  to="/dashboard/assetList"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex text-blue-500 items-center justify-start mt-8 gap-1"
+                      : "flex items-center justify-start mt-8 gap-1"
+                  }
+                >
+                 
+                  <FaClipboardList /> Asset List
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/addAsset">Add Asset</NavLink>
+                <NavLink
+                  to="/dashboard/addAsset"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "flex text-blue-500 items-center justify-start mt-8 gap-1"
+                      : "flex items-center justify-start mt-8 gap-1"
+                  }
+                >
+                  {" "}
+                  <MdAddToPhotos /> Add Asset
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/allRequest">All Request</NavLink>
+                <NavLink
+                  to="/dashboard/allRequest"
+                   className={({ isActive }) =>
+                    isActive
+                      ? "flex text-blue-500 items-center justify-start mt-8 gap-1"
+                      : "flex items-center justify-start mt-8 gap-1"
+                  }
+                >
+                  <FaWallet /> All Request
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/employeeList">Employee List</NavLink>
+                <NavLink
+                  to="/dashboard/employeeList"
+                   className={({ isActive }) =>
+                    isActive
+                      ? "flex text-blue-500 items-center justify-start mt-8 gap-1"
+                      : "flex items-center justify-start mt-8 gap-1"
+                  }
+                >
+                  <IoPeople /> Employee List
+                </NavLink>
               </li>
               <li>
-                <NavLink to="/dashboard/addEmployee">Add Employee</NavLink>
+                <NavLink
+                  to="/dashboard/addEmployee"
+                   className={({ isActive }) =>
+                    isActive
+                      ? "flex text-blue-500 items-center justify-start mt-8 gap-1"
+                      : "flex items-center justify-start mt-8 gap-1"
+                  }
+                >
+                  <IoPersonAddSharp /> Add Employee
+                </NavLink>
               </li>
-              
             </>
           ) : (
             <>
@@ -37,17 +101,14 @@ const Dashboard = () => {
               <li>
                 <NavLink to="/dashboard/requestAsset">Request an Asset</NavLink>
               </li>
-              
-             
             </>
           )}
-
-          {/* shared links */}
-          <div className="divider"></div>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
         </ul>
+        <div className=" -ml-24">
+          <button onClick={handleLogOut} className="text-white flex items-center justify-start  gap-1 text-xl font-semibold">
+            <RiLogoutBoxRFill /> Logout
+          </button>
+        </div>
       </div>
       <div className="flex-1">
         <Outlet></Outlet>
